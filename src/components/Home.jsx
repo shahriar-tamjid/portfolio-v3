cd87caf506ab3f5f0d49216be5a62cec24a9c047
Shahriar Tamjid <65736523+shahriar-tamjid@users.noreply.github.com>
2025-04-17 15:35:56 +0600

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
             </Link>
             {/* ==================== Resume ==================== */}
             <a
-              href="/Shahriar_Tamjid_Resume.pdf"
+              href="/Resume_Shahriar_Tamjid.pdf"
               className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
               download={true}
               target="_blank"
